abc42fcfb176e97f0cf383aee3d10a5dac170068
aleklisi <sebaczechu123@gmail.com>
2018-03-09 18:15:44 +0100

    WhiteSpaces class

---
+++ SQLSkaner/WhiteSpaces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class WhiteSpaces : IKeyWords
+{
+    public bool IsPartialMatch(string input)
+    {
+        return false;
+    }
+
+    public bool IsFullMatch(string input)
+    {
+        if (input.Equals(" "))
+            return true;
+        if (input.Equals("\t"))
+            return true;
+        if (input.Equals("\n"))
+            return true;
+        return false;
+    }
+
+}
