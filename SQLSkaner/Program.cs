a94d24e6a8e7a1022bcd4aee1a05633a31093c42
aleklisi <sebaczechu123@gmail.com>
2018-03-10 07:16:11 +0100

    adding read from file

--- SQLSkaner/Program.cs
+++ SQLSkaner/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace SQLSkaner
 {
     class Program
     {
+        string ReadFromFile(string fileName)
+        {
+            try
+            {   
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    String line = sr.ReadToEnd();
+                    return line;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                return e.Message;
+            }
+        }
         static void Main()
         {
             var testSkaner = new Skaner("sum");
