b9addf5084d326ef6de3e69f2269678d5eaf5957
natamate <naciaa.nm@gmail.com>
2018-03-10 10:12:03 +0100

    correct if statemtent

--- SQLSkaner/IKeyWord/Comparator.cs
+++ SQLSkaner/IKeyWord/Comparator.cs
@@ -1,27 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SQLSkaner
+﻿namespace SQLSkaner.IKeyWord
 {
     class Comparator : IKeyWords
     {
         public bool IsFullMatch(string input)
         {
             input = input.ToUpper();
-            if ((input == ">") || (input == "<") || (input == "=") 
-                || (input == ">=") || (input == "<=") || (input == "<>")
-                || (input == "BETWEEN") || (input == "LIKE") || (input == "IN"))
+            if (input == ">")
                 return true;
+            if (input == "<")
+                return true;
+            if (input == "=")
+                return true;
+            if (input == ">=")
+                return true;
+            if (input == "<=")
+                return true;
+            if (input == "<>")
+                return true;
+            if (input == "BETWEEN") 
+                return true;
+           if  (input == "LIKE")
+                return true;
+            if (input == "IN")
+                return true;
+
             return false;
         }
 
         public bool IsPartialMatch(string input)
         {
             input = input.ToUpper();
-            if ((input == ">") || (input == "<") || (input == "="))
+            if (input == ">")
+                return true;
+            if (input == "<")
+                return true;
+            if (input == "=")
+                return true;
+            if (PartialMatching.IsPartialMatchToRegex("BETWEEN", input))
+                return true;
+            if (PartialMatching.IsPartialMatchToRegex("LIKE", input))
+                return true;
+            if (PartialMatching.IsPartialMatchToRegex("IN", input))
                 return true;
             return false;
         }
