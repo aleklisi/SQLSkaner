8321e56a9a89c01c5b9ff333f69757e1c3c8c46c
natamate <naciaa.nm@gmail.com>
2018-03-10 08:51:07 +0100

    Add Simple Implementation

---
+++ SQLSkaner/ClosingBracket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLSkaner
+{
+    class ClosingBracket : IKeyWords
+    {
+        public bool IsFullMatch(string input)
+        {
+            return (input == ")");
+        }
+
+        public bool IsPartialMatch(string input)
+        {
+            return false;
+        }
+
+        public string KeyWordName()
+        {
+            return "ClosingBracket";
+        }
+    }
+}
---
+++ SQLSkaner/OpeningBracket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLSkaner
+{
+    class OpeningBracket : IKeyWords
+    {
+        public bool IsFullMatch(string input)
+        {
+            return (input == "(");
+        }
+
+        public bool IsPartialMatch(string input)
+        {
+            return false;
+        }
+
+        public string KeyWordName()
+        {
+            return "OpeningBracket";
+        }
+    }
+}
