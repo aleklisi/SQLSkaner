e431581a2905ac1c1027def997be15840ac583bf
natamate <naciaa.nm@gmail.com>
2018-03-10 11:56:53 +0100

    AliasingColumns

---
+++ SQLSkaner/IKeyWord/AliasingColumns.cs
@@ -0,0 +1,20 @@
+﻿namespace SQLSkaner.IKeyWord
+{
+    class AliasingColumns : IKeyWords
+    {
+        public bool IsFullMatch(string input)
+        {
+            return ((input.ToUpper()) == "AS");
+        }
+
+        public bool IsPartialMatch(string input)
+        {
+            return PartialMatching.IsPartialMatchToRegex("AS", input);
+        }
+
+        public string KeyWordName()
+        {
+            return "AliasingColumns";
+        }
+    }
+}
